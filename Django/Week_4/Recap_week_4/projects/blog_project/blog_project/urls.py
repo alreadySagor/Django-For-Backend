79f09cd0a3a8cf12f626c8b6f141300a97095b81
Sagor Ahmed <summyalsagor8722@gmail.com>
2025-11-10 19:12:06 +0600

    added

---
+++ Django/Week_4/Recap_week_4/projects/blog_project/blog_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name = "homepage"),
+    path('author/', include("author.urls")),
+    path('profile/', include("profiles.urls")),
+    path('category/', include("category.urls")),
+    path('post/', include("post.urls")),
+]
