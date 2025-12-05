4e678a2a4dfeff8beb89e95f528adcce9c6f2c9c
Sagor Ahmed <summyalsagor8722@gmail.com>
2025-12-05 21:53:19 +0600

    added

--- Django/Week_5/Module_19/project/app/urls.py
+++ Django/Week_5/Module_19/project/app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    # path('', views.home),
+    path('', views.set_session),
     path('get/', views.get_cookie),
+    path('get_session/', views.get_session),
     path('delete_cookie/', views.delete_cookie),
+    path('delete_session/', views.delete_session),
 ]
--- Django/Week_5/Module_19/project/app/views.py
+++ Django/Week_5/Module_19/project/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from datetime import datetime, timedelta
+from django.http import HttpResponse
 # Create your views here.
 def home(request):
     response = render(request, 'home.html')
@@ -17,3 +18,29 @@ def delete_cookie(request):
     response = render(request, 'delete_cookie.html')
     response.delete_cookie('name')
     return response
+
+def set_session(request):
+    data = {
+        'name' : 'rahim',
+        'age' : 23,
+        'language' : 'Bangla',
+    }
+    # print(request.session.get_session_cookie_age()) # eta second er hisab kore (koto second por sesh hobe seta)
+    # print(request.session.get_expiry_date()) # koto din por jinish ta sesh hobe seta dekhlam. (ar eta date show kore, kobe sesh hobe seta)
+    # request.session.update(data)
+    request.session['name'] = 'Karim'
+    return render(request, 'home.html')
+
+def get_session(request):
+    if 'name' in request.session: # 10 seconds somoy dhore nam ta theke jabe then 10s por delete hoye gele else er moddhe dhuke else er kaj korbe
+        name = request.session.get('name', 'Guest') # delete korar por nam na thakle Guest lekha ashbe (default vabe None thake)
+        age = request.session.get('age')
+        request.session.modified = True #
+        return render(request, 'get_session.html', {'name': name, 'age' : age})
+    else:
+        return HttpResponse('Your session has been expired. Login again')
+def delete_session(request):
+    # del request.session['name'] # kono ekti data like name delete korte chaile
+    
+    request.session.flush() # sob kichu delete korte chaile
+    return render(request, 'delete_session.html')
