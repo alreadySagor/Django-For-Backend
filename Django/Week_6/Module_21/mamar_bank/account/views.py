977ba0ddde01ff7511769d84c55d0ca4c2642a88
Sagor Ahmed <summyalsagor8722@gmail.com>
2025-12-17 12:27:46 +0600

    added all codes of module_21

---
+++ Django/Week_6/Module_21/mamar_bank/account/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from . forms import UserRagistrationForm
+from django.contrib.auth import login, logout
+
+# Create your views here.
+class UserRegistrationView(FormView):
+    template_name = ""
+    form_class = UserRagistrationForm
+    success_url = ""
+
+    def form_valid(self, form):
+        user = form.save() # je 3 ta model create korechi sei 3 ta model e data save hoye jabe. (save function call korechi ar return value hisebe our_user er pacchi)
+        login(user) # user er data gula diye login kore dilam. (registation form theke prapto data gula diye sorasori login kore dilam)
+        return super().form_valid(form) # form_valid function ta call hobe jodi sob thik thake
+    
