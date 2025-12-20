0c63ef925480f909516060f4a90e7777849b1f56
Sagor Ahmed <summyalsagor8722@gmail.com>
2025-12-20 19:06:03 +0600

    added

---
+++ Django/Week_6/Recap_week_6/Module_21/bank/account/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from .models import UserBankAccount, UserAddress, User
+from .constants import ACCOUNT_TYPE, GENDER_TYPE
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRagistrationForm(UserCreationForm):
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    gender = forms.ChoiceField(choices=GENDER_TYPE)
+    account_type = forms.ChoiceField(choices=ACCOUNT_TYPE)
+    street_address = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    postal_code = forms.IntegerField()
+    country = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'account_type', 'birth_date', 'gender', 'street_address', 'postal_code', 'city', 'country']
+
+        def save(self, commit = True):
+            our_user = super().save(commit = False)
+            if commit == True:
+                our_user.save()
+                account_type = self.cleaned_data.get('account_type')
+                gender = self.cleaned_data.get('gender')
+                postal_code = self.cleaned_data.get('postal_code')
+                country = self.cleaned_data.get('country')
+                birth_date = self.cleaned_data.get('birth_date')
+                city = self.cleaned_data.get('city')
+                street_address = self.cleaned_data.get('street_address')
+
+                UserAddress.objects.create(
+                    user = our_user,
+                    postal_code = postal_code,
+                    country = country,
+                    city = city,
+                    street_address = street_address,
+                )
+                UserBankAccount.objects.create(
+                    user = our_user,
+                    account_type = account_type,
+                    gender = gender,
+                    birth_date = birth_date,
+                    account_no = 1000000 + our_user.id,
+                )
+            return our_user
+        
+        def __init__(self, *args, **kwargs):
+            super().__init__
+            
--- Django/Week_6/Recap_week_6/Module_21/bank/account/urls.py
+++ Django/Week_6/Recap_week_6/Module_21/bank/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('register/', views.UserRegistrationView),
+    # path('register/', views.UserRegistrationView),
 ]
