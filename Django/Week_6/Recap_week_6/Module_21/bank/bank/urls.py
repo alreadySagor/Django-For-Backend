a0c529f6b43d34ef9f0ed9f53645eda37ea90f68
Sagor Ahmed <summyalsagor8722@gmail.com>
2025-12-21 15:16:49 +0600

    added

--- Django/Week_6/Recap_week_6/Module_21/bank/bank/urls.py
+++ Django/Week_6/Recap_week_6/Module_21/bank/bank/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name="home"),
     path('account/', include("account.urls")),
 ]
