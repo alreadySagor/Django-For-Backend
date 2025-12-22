9a3142dc87deb6399aa997eb83f03196b4322502
Sagor Ahmed <summyalsagor8722@gmail.com>
2025-12-22 10:55:50 +0600

    added

---
+++ Django/Week_6/Module_22/mamar_bank/transaction/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Transaction
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount', 'transaction_type']
+    
+    def __init__(self, *args, **kwargs):
+        self.account = kwargs.pop('account') # keyword argument er moddhe built in kichu jinish ke pass kore dibo
+        super.__init__(*args, **kwargs)
+        self.fields['tranaction_type'].disabled = True # ei field disable thakbe
+        self.fields['tranaction_type'].widget = forms.HiddenInput() # user er theke hide kora thakbe
+
+    def save(self, commit = True):
+        self.instance.account = self.account
+        self.instance.balance_after_transaction = self.account.balance # suppouse 0(main balance)--> 5000(deposite) = 5000(total_balance/balance_after_transaction)
+        return super().save()
+    
+
+class DepositeForm(TransactionForm):
+    def clean_amount(self): # amount field ke filter korbo | clean_amount --> builtin ekta function (clean_... jei field ta niye kaj korte chai sei field ta use korbo, jenehtu amount niye kaj kortechi tai amount niyechi)
+        min_deposite_amount = 100
+        amount = self.cleaned_data.get('amount') # user er fillup kora form theke amount field er value ke niye ashlam
+        if amount < min_deposite_amount:
+            raise forms.ValidationError( # raise hocche error ke show koranor jonno ekta keyword
+                f'You need to deposite at least {min_deposite_amount} $'
+            )
+        return amount
+    
+class WithdrawForm(TransactionForm):
+    def clean_amount(self):
+        account = self.account # je user request korteche tar account
+        min_withdraw_amount = 500
+        max_withdraw_amount = 20000
+        balance = account.balance
+        amount = self.cleaned_data.get('amount')
+        if amount < min_withdraw_amount:
+            raise forms.ValidationError(
+                f'You can withdraw at least {min_withdraw_amount} $'
+            )
+        if amount > max_withdraw_amount:
+            raise forms.ValidationError(
+                f'You can withdraw at most {max_withdraw_amount} $'
+            )
+        if amount > balance:
+            raise forms.ValidationError(
+                f'You have {balance} $ in your account. '
+                'You can not withdraw more than your account balance'
+            )
+        return amount
