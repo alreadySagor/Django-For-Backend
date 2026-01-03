4ce43cb200b9ebeaf045d71a10c444a8810b6bb5
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-01-03 12:36:52 +0600

    added

--- Django/Week_6/Module_22/mamar_bank/transaction/constants.py
+++ Django/Week_6/Module_22/mamar_bank/transaction/constants.py
@@ -1,10 +1,10 @@
-DEPOSITE = 1
+DEPOSIT = 1
 WITHDRAWAL = 2
 LOAN = 3
 LOAN_PAID = 4
 
 TRANSACTION_TYPE = (
-    (1, 'Deposite'),
+    (1, 'Deposit'),
     (2, 'Withdrawal'),
     (3, 'Loan'),
     (4, 'Loan Paid'),
--- Django/Week_6/Module_22/mamar_bank/transaction/forms.py
+++ Django/Week_6/Module_22/mamar_bank/transaction/forms.py
@@ -5,7 +5,8 @@ class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ['amount', 'transaction_type']
-    
+    # Amra jokhon transaction niye kaj korte jabo thokhon user er account ta ekhane pass kore dibo. Ar ei TransactionForm ke inherite korbo (DepositForm, WithdrawForm, LoanRequestForm ei class gula TransactionForm ke inheite korbe)
+    # Account ta je pop hocche ba ei jinish ta actually kothay use hocche seta amra dekhte parbo (views.py te get_form_kwargs er moddhe)
     def __init__(self, *args, **kwargs):
         self.account = kwargs.pop('account') # account value ke pop kore anlam. | keyword argument er moddhe built in kichu jinish ke pass kore dibo
         super.__init__(*args, **kwargs) # Parent class er __init__ ke overwrite korbo
@@ -13,18 +14,19 @@ class TransactionForm(forms.ModelForm):
         self.fields['tranaction_type'].widget = forms.HiddenInput() # user er theke hide kora thakbe
 
     def save(self, commit = True):
-        self.instance.account = self.account
-        self.instance.balance_after_transaction = self.account.balance # suppouse 0(main balance)--> 5000(deposite) = 5000(total_balance/balance_after_transaction)
-        return super().save()
+        self.instance.account = self.account # je user ta request korteche tar kono object jodi amader database e thake tahole sei instance er account je jabo, giye self.account ke rekhe dibo
+        # jekono trasaction er por balance take update kore dibo.
+        self.instance.balance_after_transaction = self.account.balance # suppouse 0(main balance)--> 5000(deposit) = 5000(total_balance/balance_after_transaction)
+        return super().save() # parent class (ModelForm) er save function ke call kore dilam
     
 
-class DepositeForm(TransactionForm):
-    def clean_amount(self): # amount field ke filter korbo | clean_amount --> builtin ekta function (clean_... jei field ta niye kaj korte chai sei field ta use korbo, jenehtu amount niye kaj kortechi tai amount niyechi)
-        min_deposite_amount = 100
+class DepositForm(TransactionForm):
+    def clean_amount(self): # amount field ke filter korbo | clean_amount --> builtin ekta function (clean_... jei field ta niye kaj korte chai sei field ta use korbo, jehetu amount niye kaj kortechi tai amount niyechi)
+        min_deposit_amount = 100
         amount = self.cleaned_data.get('amount') # user er fillup kora form theke amount field er value ke niye ashlam
-        if amount < min_deposite_amount:
+        if amount < min_deposit_amount:
             raise forms.ValidationError( # raise hocche error ke show koranor jonno ekta keyword
-                f'You need to deposite at least {min_deposite_amount} $'
+                f'You need to deposit at least ${min_deposit_amount}'
             )
         return amount
     
@@ -37,15 +39,15 @@ class WithdrawForm(TransactionForm):
         amount = self.cleaned_data.get('amount')
         if amount < min_withdraw_amount:
             raise forms.ValidationError(
-                f'You can withdraw at least {min_withdraw_amount} $'
+                f'You can withdraw at least ${min_withdraw_amount}'
             )
         if amount > max_withdraw_amount:
             raise forms.ValidationError(
-                f'You can withdraw at most {max_withdraw_amount} $'
+                f'You can withdraw at most ${max_withdraw_amount}'
             )
         if amount > balance:
             raise forms.ValidationError(
-                f'You have {balance} $ in your account. '
+                f'You have ${balance} in your account.'
                 'You can not withdraw more than your account balance'
             )
         return amount
@@ -55,3 +57,7 @@ class LoanRequestForm(TransactionForm):
     def clean_amount(self):
         amount = self.cleaned_data.get('amount')
         return amount
+
+# Loan koybar neya hoyche seta views er moddhe korechi. Koybar loan neya hoyeche eta model er sathe connected ba model theke pabo. Tai ei kaj ta form er moddhe na kore views er moddhe korechi karon form er kaj form e ar model er kaj views e koratai better.
+# form er kaj form e rakhte hoy
+# model er kaj views e rakhte hoy.
