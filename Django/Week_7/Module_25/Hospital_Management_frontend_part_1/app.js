db39f656d975ee3fdfa1524eb62553a21956366c
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-02-12 14:34:50 +0600

    added

--- Django/Week_7/Module_25/Hospital_Management_frontend_part_1/app.js
+++ Django/Week_7/Module_25/Hospital_Management_frontend_part_1/app.js
@@ -113,7 +113,32 @@ const handleSearch = () => {
 }
 
 
+const loadReview = () => {
+    fetch("https://testing-8az5.onrender.com/doctor/review/")
+    .then((res) => res.json())
+    .then((data) => displayReview(data));
+};
+
+const displayReview = (reviews) => {
+    reviews.forEach((review) => {
+        const parent = document.getElementById("review-container");
+        const div = document.createElement("div");
+        div.classList.add("review-card");
+        div.innerHTML = `
+            <img src="./images/girl.png" alt="">
+            <h4>${review.reviewer}</h4>
+            <p>
+                ${review.body.slice(0, 100)}
+            </p>
+            <h6>${review.rating}</h6>
+        `;
+        parent.appendChild(div);
+    });
+};
+
+
 loadServices();
 loadDoctors();
 loadDesignation();
 loadSpecialization();
+loadReview()
