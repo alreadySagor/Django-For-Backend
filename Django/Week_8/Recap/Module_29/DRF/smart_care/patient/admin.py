0a26ff12bea8b86bc3d268b8b43bfc4a71e789e2
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-03-07 09:05:39 +0600

    added

--- Django/Week_8/Recap/Module_29/DRF/smart_care/patient/admin.py
+++ Django/Week_8/Recap/Module_29/DRF/smart_care/patient/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'mobile_no', 'image'] # nicher function gular name use korte hobe ekhane
+
+    def first_name(self, obj): # name gula nijer moto kore dite parbo
+        return obj.user.first_name # patient er sathe user model er connection ache tai user model er kache giye tar first name ke ber kore anlam
+    
+    def last_name(self, obj):
+        return obj.user.last_name
+
+admin.site.register(models.Patient, PatientAdmin)
