34abbdf8a92670bbfd4113c78fd54d6390d75ad1
Sagor Ahmed <summyalsagor8722@gmail.com>
2026-04-07 15:58:32 +0600

    added all codes on 7th April 2026

--- Django/Week_8/Recap/Module_29/DRF/smart_care/doctor/admin.py
+++ Django/Week_8/Recap/Module_29/DRF/smart_care/doctor/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.AvailableTime)
+
+class specializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', ), }
+
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', ), }
+
+admin.site.register(models.Designation, DesignationAdmin)
+admin.site.register(models.Specialization, specializationAdmin)
+admin.site.register(models.Doctor)
+admin.site.register(models.Review)
